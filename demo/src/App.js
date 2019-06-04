9886732158df66e4df0946b041246d19463f903d
haoxin_qiu <haoxin_qiu@kingdee.com>
2019-06-04 14:35:21 +0800

    commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -3,16 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 // import SlideNav from '../../src/components/SlideNav';
 import {SlideNav} from '../../dist/main.min.js';
-
+import CommonSer from '../images/commonSer.png';
 class App extends Component {
+  constructor(props){
+  	super(props);
+  	this.data=[
+        {id:0,name:'机器人管理',img:require('../images/commonSer.png')},
+        {id:1,name:'技能管理',img:require('../images/createtime.png')},
+        {id:2,name:'意图管理',img:require('../images/creator.png')},
+        {id:3,name:'用户管理',img:require('../images/train_intention.png')},
+  	]
+  }
   render() {
     return (
       <div className="App">
          <div>test</div>
-         <SlideNav />
+         <SlideNav data={this.data}/>
+         <img src={CommonSer}/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/SlideNav/index.js
+++ src/components/SlideNav/index.js
@@ -6,15 +6,33 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import ClassNames from 'class-names';
 import './index.less';
 
+const prefixCls='kd-sn';
 class SlideNav extends React.Component{
     constructor(props){
     	super(props);
+    	this.data=[
+            {id:0,name:'qiuhaoxin'},
+            {id:1,name:'qiuqiuling'},
+            {id:2,name:'qiuhaorong'},
+    	]
+    }
+    renderNav=()=>{
+    	const {data}=this.props;
+        const str=data.map(item=><li key={item.id}><img src={item.img}/><span>{item.name}</span></li>)
+    	return <ul>
+            {str}
+    	</ul>
     }
     render(){
-    	return <div>
-             test
+    	const {className,style}=this.props;
+    	const classNames=ClassNames({
+           [className]:!!className,
+    	},`${prefixCls}-wrapper`);
+    	return <div className={classNames} style={style}>
+             {this.renderNav()}
     	</div>
     }
 }
